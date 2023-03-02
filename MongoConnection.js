62738c1de11ad5b744e4c0e9168d3c20eb9e452e
OsamaAtiq13 <osama@cybernest.com>
2023-03-02 11:12:49 +0500

    Second Commit

--- MongoConnection.js
+++ MongoConnection.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
-
-const uri =
-  "mongodb+srv://osama:123@mernprojectcluster.hwffsui.mongodb.net/?retryWrites=true&w=majority";
-  module.exports=async function connectDB() {
+require('dotenv').config();
+module.exports = async function connectDB() {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
   } catch (err) {
     console.log(err);
   }
-}
+};
